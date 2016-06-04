23efe01ac6fb03372b730ab5419f2c2b4750fcd0
Jon Stroop <jpstroop@gmail.com>
2016-06-04 11:19:55 -0400

    adds a cache to the magic characterizer

--- loris/helpers/classproperty.py
+++ loris/helpers/classproperty.py
@@ -1,6 +1,19 @@
 # use this as a decorator
-class classproperty(object):
-    def __init__(self, getter):
-        self.getter = getter
-    def __get__(self, instance, owner):
-        return self.getter(owner)
+# class classproperty(object):
+    # def __init__(self, getter):
+    #     self.getter = getter
+    # def __get__(self, instance, owner):
+    #     return self.getter(owner)
+class classproperty(dict):
+    def __init__(self, func):
+        print("*"*80)
+        print("Initialized " + str(func))
+        print("*"*80)
+        self.func = func
+
+    def __call__(self, *args):
+        return self[args]
+
+    def __missing__(self, key):
+        result = self[key] = self.func(*key)
+        return result
