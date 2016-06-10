932b11f43b4177c4ffb87266fb1635c6e3590276
Jon Stroop <jpstroop@gmail.com>
2016-06-10 19:56:28 -0400

    routes initialized w/ compliance (#19)

--- loris/handlers/image_handler.py
+++ loris/handlers/image_handler.py
@@ -1,5 +1,9 @@
 from tornado.web import RequestHandler
 
 class ImageHandler(RequestHandler):
+
+    def initialize(self, compliance):
+        self.compliance = compliance
+
     def get(self, identifier, region, size, rotation, quality, format):
         self.write('ImageHandler: ' + repr(self.request.uri))
