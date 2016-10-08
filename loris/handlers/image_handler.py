a9c47d8c7362997a51f3961491aba2c49627ba45
Jon Stroop <jpstroop@gmail.com>
2016-10-08 09:15:01 -0400

    just some notes about next steps

--- loris/handlers/image_handler.py
+++ loris/handlers/image_handler.py
@@ -6,25 +6,18 @@ logger = getLogger('loris')
 class ImageHandler(object):
     exposed = True
     def GET(self, identifier, iiif_params):
+        # image_request = ImageRequest(identifier, iiif_params)
+        # if etag matches:
+        #   304
+        # elif iiif_params != image_request.canoncial
+        #   303 to '/'.join((identifier, iiif_params))
+        # else
+        #   look up the right transcoder using image_request.file_format:
+        #   transcoder = IIIFRequest.transcoders[image_request.file_format]
+        #   and:
+        #   return transcoder.transcode(image_request)
+        #   which should return bytes, I believe
+        # All of this should be wrapped in a try that handles LorisExceptions
+        # similar to the InfoHandler (can probably push the etag and error
+        # handling up to a shared Mixin.)
         return 'This must be an image request. id: {0}, Params: {1}'.format(identifier, iiif_params)
-
-
-# Old Tornado handler:
-# class ImageHandler(RequestHandler):
-#
-#     def get(self, identifier, path_params):
-#         # file_path, fmt, last_mod = resolver.resolve(identifier) # TODO
-#         # info = info_cache['file_path']# TODO: use extractor if KeyError
-#         # TODO: handle loris's errors
-#         # image_request = ImageRequest(file_path, path_params, self.compliance, info)
-#         # then pass the image_request to the appropriate transcoder
-#         # use compliance.http.features to help w/ the response
-#
-#         # Make an InfoRequest, check the etag, and then get if necessary
-#         # check the etag
-#         # if the etag doesn't make, call #fulfill
-#
-#         logger.debug(identifier)
-#         logger.debug(path_params)
-#         self.write('Identifier: ' + identifier)
-#         self.write('Path: ' + path_params)
