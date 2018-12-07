18a33bc66dbaf151421cc048f9ea202029461011
Jon Stroop <jpstroop@gmail.com>
2018-12-07 14:49:24 +0000

    Switch to Pipenv, replace gemnasium, test against xenial, add python 3.7 (#154)

--- update_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
-pip freeze | grep -v "pkg\-resources" | grep -v "Loris" > requirements.txt
-echo "*************************************************"
-echo "Note: You still need to update setup.py manually!"
-echo "*************************************************"
