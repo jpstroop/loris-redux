a916869a89851d6ee3ac55cb1347bc3db67f7c2f
Jon Stroop <jpstroop@gmail.com>
2019-11-29 08:28:19 -0500

    rewrite of handler tests

--- tests/loris/handlers/resolvers_handler_tests.py
+++ tests/loris/handlers/resolvers_handler_tests.py
@@ -1,17 +1,16 @@
 from tests.loris.handlers.base_handler_test import BaseHandlerTest
 
-from json import loads
-
 
 class TestResolversHandler(BaseHandlerTest):
     def test_resolvers_redirects(self):
-        self.getPage("/resolvers")
-        self.assertStatus(303)
-        self.assertHeader("Location", "/resolvers.json")
+        with self.app_server():
+            response = self.get("/resolvers", allow_redirects=False)
+            assert response.status_code == 303
+            assert response.headers["Location"] == "/resolvers.json"
 
     def test_resolvers_json_includes_sample_resolver(self):
-        status, headers, body = self.getPage("/resolvers.json")
-        self.assertStatus(200)
-        self.assertHeader("Content-type", "application/json")
-        resolver_list = loads(body.decode("utf-8"))
-        assert any([entry["prefix"] == "loris" for entry in resolver_list])
+        with self.app_server():
+            response = self.get("/resolvers.json")
+            assert response.status_code == 200
+            assert response.headers["Content-type"] == "application/json"
+            assert any([entry["prefix"] == "loris" for entry in response.json()])
